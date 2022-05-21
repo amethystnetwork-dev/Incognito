9779704857e1ec61e797a0cc703aa0eab29e7f2a
IDontCodee <94655562+IDontCodee@users.noreply.github.com>
2022-05-21 16:46:22 -0500

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,21 @@
+var cacheName = 'incog';
+var filesToCache = [
+  '/sw.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
