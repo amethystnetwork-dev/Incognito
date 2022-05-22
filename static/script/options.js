b57dbe2dd4aa9d323d243ba54a2efe22b14a931a
IDontCodee <94655562+IDontCodee@users.noreply.github.com>
2022-05-22 15:59:00 -0500

    Update options.js

--- static/script/options.js
+++ static/script/options.js
@@ -1,14 +1,6 @@
 import { Selection } from './selection.js';
 import { Tabs } from './tabs.js';
 
-function goBlank() {
-    var page = new ABC({
-    "type": "blank"
-})
-page.setType("blank")
-page.setUrl(window.location.href)
-page.open()
-};
 async function options(app) {
     document.querySelector('#open-nav').setAttribute('data-open', '');
     app.search.title.style.display = 'block';
