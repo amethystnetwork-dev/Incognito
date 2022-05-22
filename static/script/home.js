612671d5c929f0230c51812bbe961bd0924dd4a5
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-05-22 16:08:41 -0500

    Update home.js

--- static/script/home.js
+++ static/script/home.js
@@ -17,6 +17,7 @@ function access(app) {
     app.nav.support = app.createLink('#support', 'Support');
     app.nav.apps = app.createLink('#apps', 'Apps');
     app.nav.games = app.createLink('#gs', 'Games');
+    app.nav.games = app.createLink('javascript:goBlank();', 'Go about:blank');
     app.nav.settings = app.createLink('#settings', '<i class="fas fa-sliders-h secondary"></i>', {
         id: 'apps'
     })
