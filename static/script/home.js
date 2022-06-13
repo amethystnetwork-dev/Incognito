64aa06933cebe5c17f84b47ff0c9417a64c589d9
IDontCodee <94655562+IDontCodee@users.noreply.github.com>
2022-06-13 17:31:42 -0500

    Update home.js

--- static/script/home.js
+++ static/script/home.js
@@ -4,6 +4,7 @@ const tips = [
     'You can enable about:blank tab cloaking in <a href="#settings">settings.</a>',
     'Access popular media & sites easily in <a href="#apps">apps.</a>',
     'This unofficial Incognito version is made by G&#173;it&#173;Hu&#173;b user IDo&#173;n&#173;tCod&#173;ee.',   
+    'Join our <a href="#community">discord</a> ( not official )',
     'Support Incognito at our <a href="https://www.patreon.com/incognitotn">Patreon.</a>'
 ];
 
