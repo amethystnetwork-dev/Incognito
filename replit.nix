cc58d044fc716290b9305f37b52c1fff9fbd45f1
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-06-25 20:48:39 -0500

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,10 +1,6 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-18_x
-        pkgs.brave
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.yarn
-        pkgs.replitPackages.jest
+		pkgs.nodejs
         pkgs.nano
 	];
 }
