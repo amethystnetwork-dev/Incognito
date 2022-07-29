60c863bc1500ac134f6de399ce1bfed953b1e835
David Reed <x@sys32.dev>
2022-07-28 21:18:54 -0400

    Upgrade to the latest bare server, fix invalid package name

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-import Server from 'bare-server-node';
+import createBareServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
 
-
-const bare =  new Server('/bare/', '');
+const bare = createBareServer('/bare/');
 const serve = new nodeStatic.Server('static/');
 
 const server = http.createServer();
 
-server.on('request', (request, response) => {
-    if (bare.route_request(request, response)) return true;
-    serve.serve(request, response);
+server.on('request', (req, res) => {
+	if (bare.shouldRoute(req)) {
+		bare.routeRequest(req, res);
+	} else {
+		serve.serve(req, res);
+	}
 });
 
 server.on('upgrade', (req, socket, head) => {
-	if(bare.route_upgrade(req, socket, head))return;
-	socket.end();
+	if (bare.shouldRoute(req)) {
+		bare.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
 });
 
 server.listen(process.env.PORT || 8080);
