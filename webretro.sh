14ef1d48cce9f962e4832d0fdfb0ee527fd27ea9
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-08-21 21:20:29 -0500

    Update webretro.sh

--- webretro.sh
+++ webretro.sh
@@ -5,6 +5,8 @@ echo Uncompressing webretro
 unzip webretro-v6.5.zip -d temp-webretro
 echo Copying webretro
 cp -r temp-webretro static/source/webretro
+echo Making roms folder
+mkdir static/source/webretro/roms
 echo Cleaning up
 rm -r temp-webretro
 echo Done!
