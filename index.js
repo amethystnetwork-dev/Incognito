3dd3e8d0242a09ccf7ab9dafd8f8f57b5b9b17d8
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-09-29 20:48:17 -0500

    Format index.js

--- index.js
+++ index.js
@@ -7,34 +7,32 @@ import serveStatic from "serve-static";
 console.log("Incognito\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n")
 
 const port = process.env.PORT || 8080;
-const bare =  createServer('/bare/');
+const bare = createServer('/bare/');
 const serve = serveStatic(fileURLToPath(new URL("./static/", import.meta.url)), { fallthrough: false });
 const server = http.createServer();
 
 server.on('request', (req, res) => {
   if (bare.shouldRoute(req)) {
-  bare.routeRequest(req, res);
-} else {
-  serve(req, res, (err) => {
-    res.writeHead(err?.statusCode || 500, null, {
-      "Content-Type": "text/plain",
+    bare.routeRequest(req, res);
+  } else {
+    serve(req, res, (err) => {
+      res.writeHead(err?.statusCode || 500, null, {
+        "Content-Type": "text/plain",
+      });
+      res.end(err?.stack);
     });
-    res.end(err?.stack);
-  });
-}
+  }
 });
 
 server.on('upgrade', (req, socket, head) => {
-if (bare.shouldRoute(req, socket, head)) {
-  bare.routeUpgrade(req, socket, head);
-} else {
-  socket.end();
-}
+  if (bare.shouldRoute(req, socket, head)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
 
-server.listen({
-	port: port,
-});
+server.listen({ port: port, });
 
 console.log("Server running on port " + port)
