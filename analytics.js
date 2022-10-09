bfae42028dba2dc493075dcba606d294bb6d1756
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-10-09 12:33:28 -0500

    Fonts and bad analytics

---
+++ analytics.js
@@ -0,0 +1,33 @@
+import crypto from 'node:crypto'
+
+export default function analytics(req, res, data) {
+    switch(req.url) {
+        case '/data/visit':
+            data.live++
+            data.visits++
+            if(data.peak < data.live) data.peak = data.live
+            res.end('OK')
+            break;
+        case '/data/create-id':
+            res.end(crypto.randomUUID())
+            break;
+        case '/data/keep-alive':
+            res.end()
+            break;
+        case '/data/destroy':
+            removeVisit(req, data)
+            res.end()
+        break;
+        case '/data/data':
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(data))
+            break;
+        default:
+            res.writeHead(404)
+            res.end('404')
+    }
+}
+
+function removeVisit(req, data) {
+    data.live = data.live - 1
+}
