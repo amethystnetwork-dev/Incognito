15f99e3a27869f24cd217a1343e8d902a4c269e2
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-12-04 17:11:10 -0600

    Fix

--- src/index.js
+++ src/index.js
@@ -4,34 +4,30 @@ import { createServer as createHttpsServer } from "node:https";
 import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import serveStatic from "serve-static";
-import analytics from './analytics.js';
 
 // The following message MAY NOT be removed
 console.log("Incognito\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
 
-var data = { live: 0, peak: 0, visits: 0 }
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
-var server;
+var server, PORT;
 if(existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
   server = createHttpsServer({
     key: readFileSync("../ssl/key.pem"),
     cert: readFileSync("../ssl/cert.pem")
   });
-} else server = createHttpServer();
+  PORT = 443;
+} else { server = createHttpServer(); PORT = (process.env.PORT || 8080);}
 
 server.on("request", (req, res) => {
   if(bare.shouldRoute(req)) bare.routeRequest(req, res); else {
-    if(req.url.startsWith("/data")) {
-     analytics(req, res, data)
-    } else {
     serve(req, res, (err) => {
       res.writeHead(err?.statusCode || 500, null, {
         "Content-Type": "text/plain",
       });
       res.end('Error')
     })
-  }}
+  }
 });
 
 server.on("upgrade", (req, socket, head) => {
@@ -42,12 +38,12 @@ server.on('listening', () => {
   const addr = server.address();
 
   console.log(`Server running on port ${addr.port}`)
-	console.log('');
+  console.log('');
   console.log('You can now view it in your browser.')
   /* Code for listing IPS from website-aio */
-	console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-	try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
-	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+  console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
+  try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
+  if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
 });
 
-server.listen({ port: (process.env.PORT || 8080) })
+server.listen({ port: PORT })
--- ssl/README.md
+++ ssl/README.md
@@ -2,4 +2,6 @@
 
 To enable SSL when using a provider that does not provide them, place your certificates in the SSL folder.
 
-The certificate should be named `cert.pem` and the private key named `key.pem`
+The certificate should be named `cert.pem` and the private key named `key.pem`.
+
+When SSL is enabled only port 443 will be used.
